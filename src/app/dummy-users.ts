a48d71970a2cb7822b52037c229122981f865c80
MM-WebDeveloper <milan.varnamo@gmail.com>
2025-03-08 18:27:07 +0100

    feat: add dummy data representing users

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,37 @@
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'John Doe',
+    avatar: 'user-1.jpg',
+  },
+  {
+    id: 'u2',
+    name: 'Mary Carlsen',
+    avatar: 'user-2.jpg',
+  },
+  {
+    id: 'u3',
+    name: 'Karl Dibnah',
+    avatar: 'user-3.jpg',
+  },
+  {
+    id: 'u4',
+    name: 'Joe Hendrix',
+    avatar: 'user-4.jpg',
+  },
+  {
+    id: 'u5',
+    name: 'Lucy Gallagher',
+    avatar: 'user-5.jpg',
+  },
+  {
+    id: 'u6',
+    name: 'Bob Miller',
+    avatar: 'user-6.jpg',
+  },
+  {
+    id: 'u7',
+    name: 'Elisabeth Miller',
+    avatar: 'user-7.jpg',
+  },
+];
